80ce25b385cdb01074066c2a9db078c67c72d775
Lennox2018 <43885619+Lennox2018@users.noreply.github.com>
2019-04-29 20:28:03 +0800

    Import the points

---
+++ Cluster201904.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+xy=open("xy.coordinates",'r')
+pointsdic={}
+# seperate data into items
+pointslist = xy.read()
+pointslist=pointslist.replace('\n',',')
+pointslist=pointslist.split(',')
+for i in range(0,len(pointslist)):
+    if i%3 == 0:
+        temp=pointslist[i]
+    elif i%3 == 1:    
+        x=pointslist[i]
+    else:   # y
+        pointsdic[temp]=(float(x),float(pointslist[i])
+print(pointsdic)        
+
+
+close(xy)
