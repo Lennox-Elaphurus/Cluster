664f94ac9306c76d38fc6451960afaebae2ea579
Lennox2018 <43885619+Lennox2018@users.noreply.github.com>
2019-04-30 19:58:04 +0800

    version0.3
    input finished

--- Cluster201904.py
+++ Cluster201904.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
-import pylint
 
-xy=open("xy.coordinates",'r')
-pointsdic={}
-# seperate data into items
+
+xy = open("xy.coordinates", 'r')
+pointsdic = {}
+# separate data into items
 pointslist = xy.read()
-pointslist=pointslist.replace('\n',',')
-pointslist=pointslist.split(',')
-for i in range(0,len(pointslist)):
-    if i%3 == 0:
-        temp=pointslist[i]
-    elif i%3 == 1:    
-        x=pointslist[i]
+pointslist = pointslist.replace('\n', ',')
+pointslist = pointslist.split(',')
+for i in range(0, len(pointslist)):
+    if i % 3 == 0:
+        temp = pointslist[i]
+    elif i % 3 == 1:
+        x = pointslist[i]
     else:   # y
-        pointsdic[temp]=(float(x),float(pointslist[i])
-print(pointsdic)      
-
-
-close(xy)
+        pointsdic[temp] = (float(x), float(pointslist[i]))
+print(pointsdic)
+xy.close()
