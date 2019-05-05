dbe5a8151f4cc01f7eaddaa478a96f946e7c0ada
Lennox2018 <43885619+Lennox2018@users.noreply.github.com>
2019-05-05 16:02:10 +0800

    Designing test.txt

---
+++ producingtest.py
@@ -0,0 +1,8 @@
+import random as ra
+print(ra.randint(0,100))
+
+output=open("test.txt",'w')
+output=txt.write()
+for i in range(0,100):
+    output.write(str(i)+","+str(ra.randint(0,100))+","+str(ra.randint(0,100)+"\n")
+
